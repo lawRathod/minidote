eab33702a86b293ed2583699b49176c44b389461
Philipp Lersch <plersch@rptu.de>
2025-05-19 12:18:24 +0000

    Example syntax fixes

--- ex3/vcgenserver/lib/exampleclient.ex
+++ ex3/vcgenserver/lib/exampleclient.ex
@@ -1,6 +1,6 @@
 defmodule VCGenClient do
 
-    def exampleUsage () {
+    def exampleUsage () do
         # Start the server
         {:ok, pid} = GenServer.start_link(VCGenServer, "Hello, World")
 
@@ -9,11 +9,11 @@ defmodule VCGenClient do
         #=> "hello"
 
         # Send a message without return (async)
-        ^:ok = GenServer.cast(pid, {:push, "elixir"})
+        :ok = GenServer.cast(pid, {:push, "elixir"})
         #=> :ok
 
         # Send another message and expect a return value (sync)
         result2 = GenServer.call(pid, :pop)
         #=> "elixir"
-    }
+    end
 end
